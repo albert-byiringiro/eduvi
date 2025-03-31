04b94444f2728bf3eb4c0da37aa9bdab5c466dab
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 14:15:05 +0200

    feat(auth): add sign up route in controllers [refs #9]

--- apps/backend/src/iam/authentication/authentication.controller.ts
+++ apps/backend/src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+}
