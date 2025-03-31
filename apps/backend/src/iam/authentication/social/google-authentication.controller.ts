695be13c4847c80f7b7fc789b291dfd3711b59e1
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 23:06:12 +0200

    refactor(auth): add Google authentication controller [refs #11]

--- apps/backend/src/iam/authentication/social/google-authentication.controller.ts
+++ apps/backend/src/iam/authentication/social/google-authentication.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GoogleAuthenticationService } from './google-authentication.service';
+import { GoogleTokenDto } from '../dto/google-token.dto';
 
 @Controller('google-authentication')
-export class GoogleAuthenticationController {}
+export class GoogleAuthenticationController {
+  constructor(
+    private readonly googleAuthService: GoogleAuthenticationService,
+  ) {}
+
+  @Post()
+  authenticate(@Body() tokenDto: GoogleTokenDto) {
+    return this.googleAuthService.authenticate(tokenDto.token);
+  }
+}
