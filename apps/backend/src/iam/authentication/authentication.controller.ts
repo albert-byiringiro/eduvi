5fb7a5ae43957d8463ffb815c028a023baba039a
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 14:16:47 +0200

    feat(auth): add sign in route in controllers [refs #9]

--- apps/backend/src/iam/authentication/authentication.controller.ts
+++ apps/backend/src/iam/authentication/authentication.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -9,5 +10,11 @@ export class AuthenticationController {
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
   }
 }
