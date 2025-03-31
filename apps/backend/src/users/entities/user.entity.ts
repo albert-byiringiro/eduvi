85c1f53556fc4aeebe2f3329301107c3e22ae529
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 23:26:12 +0200

    refactor(auth): improve the user entity file  [refs #11]

--- apps/backend/src/users/entities/user.entity.ts
+++ apps/backend/src/users/entities/user.entity.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -8,12 +16,19 @@ export class User {
   @Column({ nullable: true })
   fullname: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: false })
   email: string;
 
   @Column({ nullable: true })
+  @Exclude()
   password: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, unique: true })
   googleId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
