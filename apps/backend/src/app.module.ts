bdea0c6ecee266865ed51f3324185c9097f30c56
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-03-31 15:51:12 +0200

    Revert "[TASK][AUTH] Implemented user registration and login"

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,25 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    IamModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
