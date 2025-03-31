93fbeb8d97086deb0d6db81aad4eff60adabd453
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 12:19:53 +0200

    feat(auth): add User entity within our database [refs #9]

--- apps/backend/src/users/entities/user.entity.ts
+++ apps/backend/src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullname: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}
