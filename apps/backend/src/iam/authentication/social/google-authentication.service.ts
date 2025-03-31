2872af2623df3e9004683bbca2f5a363d0e6aafa
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 22:22:58 +0200

    refactor(auth): add google authentication service module initialization [refs #11]

--- apps/backend/src/iam/authentication/social/google-authentication.service.ts
+++ apps/backend/src/iam/authentication/social/google-authentication.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import { AuthenticationService } from '../authentication.service';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  private oauthClient: OAuth2Client;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthenticationService,
+  ) {}
+
+  onModuleInit() {
+    const clientId = this.configService.get('GOOGLE_CLIENT_ID');
+    const clientSecret = this.configService.get('GOOGLE_CLIENT_SECRET');
+
+    this.oauthClient = new OAuth2Client(clientId, clientSecret);
+  }
+}
