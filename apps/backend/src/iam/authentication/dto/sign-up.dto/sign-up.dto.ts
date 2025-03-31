d80f71bb3ce769bfb774e9de4d0537dd0ef38102
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 13:38:35 +0200

    feat(auth): add validation to sign-up dto [refs #9]

--- apps/backend/src/iam/authentication/dto/sign-up.dto/sign-up.dto.ts
+++ apps/backend/src/iam/authentication/dto/sign-up.dto/sign-up.dto.ts
@@ -1,1 +1,15 @@
-export class SignUpDto {}
+import { IsString, IsEmail, MinLength, MaxLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(50)
+  fullname: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
