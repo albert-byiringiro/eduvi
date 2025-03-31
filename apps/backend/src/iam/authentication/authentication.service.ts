c6a8e6454ae18eab316bca120c1fdfc186676ddc
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-03-31 13:42:31 +0200

    feat(auth): add injecting of dependecies to the authentication service [refs #9]

--- apps/backend/src/iam/authentication/authentication.service.ts
+++ apps/backend/src/iam/authentication/authentication.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+}
