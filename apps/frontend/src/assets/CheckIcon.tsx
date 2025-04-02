4d0172dbd50d39c4ec12bd70b2940f41a9b7d82d
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-04-02 22:16:58 +0200

    feat(auth): add checkIcon component

---
+++ apps/frontend/src/assets/CheckIcon.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from 'react';
+
+interface CheckIconProps {
+  className?: string;
+}
+
+export default function CheckIcon({ className }: CheckIconProps): ReactElement {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
