9b7c1f562e9236ece6d5756f8f9dc5a0c6719acd
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-04-03 15:01:28 +0200

    feat(auth): add custom hook to disable scrolling [refs: [TASK][Auth] Build Registration Form #12]

---
+++ apps/frontend/src/components/modal/useDisableScroll.ts
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+export default function useDisableScroll() {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  });
+}
