4ed66d394d66b3d4b0168685b994e5d3401cd7d8
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-04-03 13:30:50 +0200

    feat(auth): add actual logo component [refs: [TASK][Auth] Build Registration Form #12]

--- apps/frontend/src/pages/auth/Signup.tsx
+++ apps/frontend/src/pages/auth/Signup.tsx
@@ -1,5 +1,5 @@
-import eduviLogo from '../../assets/img/eduvi-dummy-logo.png';
 import signupIllustration from '../../assets/img/signup-illustration.svg';
+import Logo from '../../assets/Logo';
 import SignupForm from '../../components/forms/SignupForm';
 
 export default function Signup() {
@@ -7,7 +7,7 @@ export default function Signup() {
     <div className="grid w-full min-h-screen grid-cols-1 bg-white lg:grid-cols-2">
       <div className="flex flex-col justify-center p-6 lg:p-12">
         <div className="mb-8 lg:mb-12">
-          <img src={eduviLogo} alt="Eduvi Logo" className="h-8 mb-4 lg:h-10 lg:mb-6" />
+          <Logo className="mb-4 lg:mb-6" />
           <h1 className="mb-4 text-3xl font-bold text-primary-900 lg:text-4xl">
             Welcome to <br />
             Eduvi Online <br />
